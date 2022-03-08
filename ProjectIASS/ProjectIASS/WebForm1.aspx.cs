b22bd1fb2f18df2d34b22a58321a929cbb1f1776
CorinaLazar27 <corina_lazar27@yahoo.com>
2022-03-08 15:27:48 +0200

    login with database

--- ProjectIASS/ProjectIASS/WebForm1.aspx.cs
+++ ProjectIASS/ProjectIASS/WebForm1.aspx.cs
@@ -5,42 +5,78 @@ using System.IO;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace ProjectIASS
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-JN36OI3\\MSSQLSERVER01;Initial Catalog=IASS;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataReader dr;
         protected void Page_Load(object sender, EventArgs e)
         {
-            string[] utilizatori = File.ReadAllLines(Server.MapPath("~/Fisiere/") + "utilizatori.txt");
-            foreach (var line in utilizatori)
+            if (!Page.IsPostBack)
             {
-                string[] inregistrare = line.Split(',');
-                DropDownList1.Items.Add(inregistrare[0]);
+                try
+                {
+                    con.Open();
+                    cmd = new SqlCommand("select NumeUtilizator from Users", con);
+                    dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        DropDownList1.Items.Add(dr[0].ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Label1.Text = "Conexiune esuata " + ex.Message;
+                }
+                finally
+                {
+                    con.Close();
+                }
             }
+
 
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            string[] utilizatori = File.ReadAllLines(Server.MapPath("~/Fisiere/") + "utilizatori.txt");
-            foreach (var line in utilizatori)
+            try
             {
-                string[] inregistrare = line.Split(',');
-
-                if ((DropDownList1.Text).Equals(inregistrare[0]))
+                con.Open();
+                cmd = new SqlCommand("select Parola from Users where NumeUtilizator='" + DropDownList1.Text +
+               "'", con);
+                dr = cmd.ExecuteReader();
+                if (!dr.Read())
                 {
-                    if (TextBox1.Text.Trim().Equals(inregistrare[1].Trim()))
+                    Label1.Text = "Datele sunt gresite!";
+                }
+                else
+                {
+                    string url;
+                    if (dr[0].ToString().Trim() == TextBox1.Text.Trim())
                     {
-                        Response.Redirect("WebForm2.aspx?utilizator=" + DropDownList1.Text.Trim());
+                        Application["numeUser"] = DropDownList1.Text;
+                        url = "WebForm2.aspx";
+                        Response.Redirect(url);
                     }
                     else
                     {
-                        string script = "alert(\"Parola incorecta!\");";
-                        ScriptManager.RegisterStartupScript(this, GetType(), "ServerControlScript", script, true);
+                        Label1.Text = "Parola gresita!";
                     }
                 }
             }
+            catch (Exception ex)
+            {
+                Label1.Text = "Nu se poate realiza conexiunea " + ex.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+
         }
     }
 }
